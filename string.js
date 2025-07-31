cd0411ebb9c0f72b4dd959bd8520b24a68e370c4
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-31 20:30:22 +0600

    string

---
+++ string.js
@@ -0,0 +1,25 @@
+const country = 'Bangladesh';
+const division = "Chittagong";
+const district = `Cox's Bazar`;
+// const thana = new String('Chakora');
+
+console.log(country);
+
+let numbers = [23, 32, 45, 67, 89, 90];
+console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[5]);
+
+//array is mutable. it can be changed.
+numbers[1] = 100;
+console.log(numbers);
+
+
+const capital= 'Dha ka';
+console.log(capital);
+// console.log(capital.length);
+// console.log(capital[0]);
+
+//string is immutable. it can't be changed
+capital[0] = 'C';
+console.log(capital);
