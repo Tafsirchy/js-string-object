e163ce2ea1c1b19e2aa574c73a7e4342f7a2b8f6
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-31 21:17:37 +0600

    case

---
+++ case.js
@@ -0,0 +1,26 @@
+const school = 'Chakoria Korak Biddyapith';
+console.log(school);
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+const subject = 'Chemistry';
+const book = 'chemIsTry';
+
+//to compare two string. make it lowercase or uppercase
+if(subject.toLowerCase === book.toLowerCase){
+    console.log('omago ami engineer');
+}
+else{
+    console.log('chemistry ki mainse pore?');
+}
+
+const drink = ' water';
+const liquid = '  water';
+
+if(drink.trim() === liquid.trim()){
+    console.log('panir opor naam jiboooon');
+}
+else{
+    console.log('somudrer pani khaite pari naaa');
+}
+ 
