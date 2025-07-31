0b28a8a0a419961eb31f062e0f5dbff76f04b3b9
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-31 23:50:33 +0600

    reverse

---
+++ reverse.js
@@ -0,0 +1,24 @@
+const sentence = 'i am learning web dev.';
+
+//const result = '.ved bew gninrael ma i';
+
+// let reverse = '';
+
+// // for(const letter of sentence){
+// //     // console.log(letter);
+// //     reverse = letter + reverse;
+// // }
+// // console.log(reverse);
+
+// let rev = ''
+// for(let i=0; i<sentence.length; i++){
+
+//     // console.log(i)
+//     // console.log(sentence[i]);
+//     const letter = sentence[i];
+//     rev = letter + rev;
+// }
+// console.log(rev);
+
+const reverse = sentence.split('').reverse().join('');
+console.log(reverse);
