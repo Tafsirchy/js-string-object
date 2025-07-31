5d11f2c82eb1d60672385a188e922ff922cf571e
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-31 22:46:55 +0600

    slice-split-join-concat-includes

---
+++ slice-split-join-concat-includes.js
@@ -0,0 +1,32 @@
+// slice 
+// const address = 'andorkillah';
+// const part = address.slice(2,8);
+// console.log(part);
+
+// split 
+// const sentance = 'i am a good and hardworking person';
+// console.log(sentance.split(' '));
+// console.log(sentance.split(''));
+// console.log(sentance.split('a'));
+
+// const friendsStr= 'Rahim,ramim, kahim, karim, jamim, jamal';
+// const friends =  friendsStr.split(',');
+// console.log(friends);
+
+// const realFriends = ['Rahim','ramim','kahim',' karim', 'jamim', 'jamal'];
+// console.log(realFriends);
+// console.log(realFriends.join());
+// console.log(realFriends.join('|'));
+// console.log(realFriends.join('-'));
+
+const first = 'Tafsir';
+const last = 'Chy';
+
+const fullNane = first + ' ' + last ;
+console.log(fullNane);
+
+// const fullName2 = first.concat(last);
+const fullName2 = first.concat(' ').concat(last);
+console.log(fullName2);
+
+console.log(last.includes('Chy'));
